f7293050d0eb0c08f75b9605fb0243fec3ad210e
aviral juyal <aviraljuyal@gmail.com>
2024-05-14 16:38:24 +0530

    minor

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Expense } from '../../models/expense';
 import { Income } from '../../models/income';
-import { MainService } from '../../services/main.service';
 
 @Component({
   selector: 'app-chart',
@@ -9,58 +8,115 @@ import { MainService } from '../../services/main.service';
   styleUrl: './chart.component.scss',
 })
 export class ChartComponent {
+  @Input() allExpenses: Expense[] = [];
+  @Input() allIncomes: Income[] = [];
+
   title = 'Expense Tracker';
   chart: any;
 
-  chartOptions = {
-    animationEnabled: true,
-    theme: 'light2',
-    title: {
-      text: 'Expense vs Income',
-    },
-    axisX: {
-      valueFormatString: 'DD/MM/YYYY',
-    },
-    axisY: {
-      title: '',
-    },
-    toolTip: {
-      shared: true,
-    },
-    legend: {
-      cursor: 'pointer',
-      itemclick: function (e: any) {
-        if (
-          typeof e.dataSeries.visible === 'undefined' ||
-          e.dataSeries.visible
-        ) {
-          e.dataSeries.visible = false;
-        } else {
-          e.dataSeries.visible = true;
-        }
-        e.chart.render();
-      },
-    },
-    data: [
-      {
-        type: 'line',
-        showInLegend: true,
-        name: 'Expense',
-        xValueFormatString: 'MMM DD, YYYY',
-        dataPoints: this.mainService.allExpenses.map((e) => {
-          return { x: new Date(e.date), y: +(e.amount ?? 0) };
-        }),
-      },
-      {
-        type: 'line',
-        showInLegend: true,
-        name: 'Income',
-        dataPoints: this.mainService.allIncomes.map((e) => {
-          return { x: new Date(e.date), y: +(e.amount ?? 0) };
-        }),
-      },
-    ],
-  };
+  chartOptions!: any;
 
-  constructor(public mainService: MainService) {}
+  ngOnInit() {
+    this.chartOptions = {
+      animationEnabled: true,
+      theme: 'light2',
+      title: {
+        text: 'Expense vs Income',
+      },
+      axisX: {
+        valueFormatString: 'DD/MM/YYYY',
+      },
+      axisY: {
+        title: '',
+      },
+      toolTip: {
+        shared: true,
+      },
+      legend: {
+        cursor: 'pointer',
+        itemclick: function (e: any) {
+          if (
+            typeof e.dataSeries.visible === 'undefined' ||
+            e.dataSeries.visible
+          ) {
+            e.dataSeries.visible = false;
+          } else {
+            e.dataSeries.visible = true;
+          }
+          e.chart.render();
+        },
+      },
+      data: [
+        {
+          type: 'line',
+          showInLegend: true,
+          name: 'Expense',
+          xValueFormatString: 'MMM DD, YYYY',
+          dataPoints: this.allExpenses.map((e) => {
+            return { x: new Date(e.date), y: +(e.amount ?? 0) };
+          }),
+        },
+        {
+          type: 'line',
+          showInLegend: true,
+          name: 'Income',
+          dataPoints: this.allIncomes.map((e) => {
+            return { x: new Date(e.date), y: +(e.amount ?? 0) };
+          }),
+        },
+      ],
+    };
+  }
+  ngOnChanges() {
+    console.log('helo');
+    this.chartOptions = {
+      animationEnabled: true,
+      theme: 'light2',
+      title: {
+        text: 'Expense vs Income',
+      },
+      axisX: {
+        valueFormatString: 'DD/MM/YYYY',
+      },
+      axisY: {
+        title: '',
+      },
+      toolTip: {
+        shared: true,
+      },
+      legend: {
+        cursor: 'pointer',
+        itemclick: function (e: any) {
+          if (
+            typeof e.dataSeries.visible === 'undefined' ||
+            e.dataSeries.visible
+          ) {
+            e.dataSeries.visible = false;
+          } else {
+            e.dataSeries.visible = true;
+          }
+          e.chart.render();
+        },
+      },
+      data: [
+        {
+          type: 'line',
+          showInLegend: true,
+          name: 'Expense',
+          xValueFormatString: 'MMM DD, YYYY',
+          dataPoints: this.allExpenses.map((e) => {
+            return { x: new Date(e.date), y: +(e.amount ?? 0) };
+          }),
+        },
+        {
+          type: 'line',
+          showInLegend: true,
+          name: 'Income',
+          dataPoints: this.allIncomes.map((e) => {
+            return { x: new Date(e.date), y: +(e.amount ?? 0) };
+          }),
+        },
+      ],
+    };
+  }
 }
--- src/app/components/piechart/piechart.component.ts
+++ src/app/components/piechart/piechart.component.ts
@@ -49,4 +49,38 @@ export class PiechartComponent {
       ],
     };
   }
+
+  ngOnChanges() {
+    this.chartOptions = {
+      animationEnabled: true,
+      theme: 'dark2',
+      exportEnabled: true,
+      title: {
+        text: this.title,
+      },
+
+      data: [
+        {
+          type: 'pie', // You can change this to any other supported type like 'column', 'line', etc.
+          indexLabel: '{name}: {y}%',
+          dataPoints: (() => {
+            const totals: Record<string, number> = {}; // Explicitly typing the object
+            this.data.forEach((expense) => {
+              if (totals[expense.option]) {
+                totals[expense.option] += +(expense.amount ?? 0);
+              } else {
+                totals[expense.option] = +(expense.amount ?? 0);
+              }
+            });
+
+            const totalExpense = this.totalData;
+            return Object.keys(totals).map((option) => ({
+              name: option,
+              y: Math.round((totals[option] / totalExpense) * 100),
+            }));
+          })(),
+        },
+      ],
+    };
+  }
 }
