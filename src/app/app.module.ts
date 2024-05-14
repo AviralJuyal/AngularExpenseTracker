a439425e33c85c42688b80065eb427214b8a30d7
aviral juyal <aviraljuyal@gmail.com>
2024-05-14 15:49:45 +0530

    removed redundant components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,12 @@ import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from '../environments/environment';
 import { HomeComponent } from './pages/home/home.component';
-import { LoginModalComponent } from './components/login-modal/login-modal.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { ExpensesComponent } from './pages/expenses/expenses.component';
 import { IncomesComponent } from './pages/incomes/incomes.component';
-import { TransactionsComponent } from './pages/transactions/transactions.component';
 import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 import { ChartComponent } from './components/chart/chart.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -25,11 +23,9 @@ import { PiechartComponent } from './components/piechart/piechart.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginModalComponent,
     SidenavComponent,
     ExpensesComponent,
     IncomesComponent,
-    TransactionsComponent,
     ChartComponent,
     CurrencyPipe,
     PiechartComponent,
