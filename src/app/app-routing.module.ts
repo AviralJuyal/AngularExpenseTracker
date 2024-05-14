1811e28ec6b0ac3df9e7908d05280c2fb26b8d0f
aviral juyal <aviraljuyal@gmail.com>
2024-05-14 15:50:11 +0530

    removed redundant components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ExpensesComponent } from './pages/expenses/expenses.component';
 import { IncomesComponent } from './pages/incomes/incomes.component';
-import { TransactionsComponent } from './pages/transactions/transactions.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: HomeComponent },
   { path: 'expenses', component: ExpensesComponent },
   { path: 'incomes', component: IncomesComponent },
-  { path: 'transactions', component: TransactionsComponent },
 
   { path: '**', redirectTo: 'dashboard' },
 ];
